33e1fd2af16a7cf81909615e33b27797fdccc8b2
gallorob <32876132+gallorob@users.noreply.github.com>
2023-06-30 12:17:56 +0100

    Handle losing players and add default face

--- face_prediction.py
+++ face_prediction.py
@@ -3,6 +3,7 @@ from model import FacialExpressionModel
 import numpy as np
 import os
 import sys
+# import pygame
 
 from configs import configs
 
@@ -11,10 +12,15 @@ model = FacialExpressionModel("model.json", "model_weights.h5")
 font = cv2.FONT_HERSHEY_SIMPLEX
 
 class VideoCamera(object):
-    def __init__(self):
+    def __init__(self, n_players=2):
         # Use correct camera index for Mac and PC ¯\_(ツ)_/¯
         self.video = cv2.VideoCapture(1 if sys.platform == 'darwin' else 0)
-
+        self.no_face = pygame.image.load('assets/noface.png')
+        self.processed_faces = [self.no_face for _ in range(n_players)]
+        self.predicted_emotions = [-1 for _ in range(n_players)]
+        self.patience = [10 for _ in range(n_players)]
+        self.bboxes_at = [-1 for _ in range(n_players)]
+        
     def __del__(self):
         self.video.release()
 
@@ -27,18 +33,33 @@ class VideoCamera(object):
         # keep faces ordered
         faces = sorted(faces, key=lambda x: -x[0])
         
-        assert faces is not None, 'No faces at all?'
-        cropped_faces = [np.zeros((512, 512)) for _ in range(2)]  # empty faces
-        predicted_emotions = [-1, -1]  # no predictions
-
-        # weird for-loop because camera can get either 0 or more faces, but we always want 2
-        for i in range(len(cropped_faces)):
-            if i < len(faces):
-                x, y, w, h = faces[i]
+        for i in range(len(self.patience)):
+            self.patience[i] -= 1
+        
+        for i, (x, y, w, h) in enumerate(faces):
+            if i < len(self.processed_faces):
                 fc = gray_fr[y:y+h, x:x+w]
                 roi = cv2.resize(fc, (48, 48))
-
-                cropped_faces[i] = cv2.resize(fc, (512, 512))
-                predicted_emotions[i] = configs.expressions.index(model.predict_emotion(roi[np.newaxis, :, :, np.newaxis]))
+                
+                cropped_face = cv2.resize(fc, (512, 512))
+                predicted_emotion = configs.expressions.index(model.predict_emotion(roi[np.newaxis, :, :, np.newaxis]))
+                
+                if self.bboxes_at[i] == -1:
+                    at = i
+                else:
+                    at = np.argmin([abs(x - y) for y in self.bboxes_at])
+    
+                self.processed_faces[at] = cropped_face
+                self.predicted_emotions[at] = predicted_emotion
+                self.patience[at] = 10
+                self.bboxes_at[at] = x
+                
+        for i in range(len(self.patience)):
+            if 0 < self.patience[i] < 10:
+                self.predicted_emotions[i] = -1
+            elif self.patience[i] <= 0:
+                self.predicted_emotions[i] = -1
+                self.processed_faces[i] = self.no_face
+                self.bboxes_at[i] = -1
         
-        return cropped_faces, predicted_emotions
+        return self.processed_faces, self.predicted_emotions
